6d79a5505f6d918bf285905adb29701f225335bf
Joe Gigs <jgiglio86@gmail.com>
2016-10-05 17:59:58 -0400

    Added user first and last name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_devise_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
   def current_bike
@@ -10,4 +11,8 @@ class ApplicationController < ActionController::Base
     @bike.safety_check.quick_release === true && @bike.safety_check.wheels_spin_right == true && @bike.safety_check.brakes == true && @bike.safety_check.chain == true && @bike.safety_check.tools == true && @bike.safety_check.tire_pressure == true && @bike.safety_check.tire_tread == true
   end
     helper_method :bike_is_safe
+
+  def configure_devise_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:fname, :lname])
+  end
 end
